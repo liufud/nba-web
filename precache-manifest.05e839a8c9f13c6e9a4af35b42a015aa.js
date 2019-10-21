70508193727ce3e0f7c6513fbf53de1440ecc0b8
Jacky <liufudijacky@hotmail.com>
2019-10-20 20:13:00 -0400

    Updates

---
+++ precache-manifest.05e839a8c9f13c6e9a4af35b42a015aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b7b7a16f2cb41dbac69cc54d5ef471",
+    "url": "/nba-web/index.html"
+  },
+  {
+    "revision": "d8bdcb5c5907fc833587",
+    "url": "/nba-web/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "9c3734ea2178be759bc1",
+    "url": "/nba-web/static/css/main.c75e3314.chunk.css"
+  },
+  {
+    "revision": "d8bdcb5c5907fc833587",
+    "url": "/nba-web/static/js/2.7f636369.chunk.js"
+  },
+  {
+    "revision": "9c3734ea2178be759bc1",
+    "url": "/nba-web/static/js/main.c6ea421d.chunk.js"
+  },
+  {
+    "revision": "dd83671244020f86a53d",
+    "url": "/nba-web/static/js/runtime-main.5116baeb.js"
+  },
+  {
+    "revision": "63bc98a9252819c9a4d2bd207dec5f50",
+    "url": "/nba-web/static/media/nav_logoman.63bc98a9.svg"
+  }
+]);
